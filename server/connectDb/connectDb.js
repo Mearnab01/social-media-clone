428eda95a30f241a1ff099ef75cd59462ad403cb
Arnab <Narnabnath919@gmail.com>
2025-03-14 21:18:05 +0530

    fix db conn error

--- server/connectDb/connectDb.js
+++ server/connectDb/connectDb.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 const conncetDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDb connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("Failed to connct to mongoDb ", error);
